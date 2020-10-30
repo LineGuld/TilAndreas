a12ad8148a56ca2a5829ec00853dd0c2b646999b
Andreas Y Andersen <andreasyoungandersen@gmail.com>
2020-10-30 14:07:27 +0100

    Added a reply

--- Hej/src/Heeeeej.java
+++ Hej/src/Heeeeej.java
@@ -3,5 +3,7 @@ public class Heeeeej
   public static void main(String[] args)
   {
     System.out.println("Whyyyy??");
+    System.out.println("'Why not' - He muttered, afraid of the void creeping around the corner. He did not know how it moved, how it could move, not even why it was moving");
+    System.out.println("Did he even know what moving meant? The questions kept piling up, more and more, just questions, no answers. Could they even be answered?");
   }
 }
